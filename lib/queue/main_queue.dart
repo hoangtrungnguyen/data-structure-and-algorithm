dbcc8db4f2d158bfbe6f526d9684e321c83ffe22
nguyen <nguyen.hoang@ohmidasvn.com>
2023-12-26 14:54:39 +0700

    for each level order in a Trees using a Queue 2

---
+++ lib/queue/main_queue.dart
@@ -0,0 +1,25 @@
+import 'package:data_structure_and_algorithm/queue/queue.dart';
+
+void main() {
+  final queue = QueueStack<String>();
+  queue.enqueue('Ray');
+  queue.enqueue('Brian');
+  queue.enqueue('Eric');
+  print(queue);
+  queue.dequeue();
+  print(queue);
+  queue.peek;
+  print(queue);
+  queue.dequeue();
+  print(queue);
+  queue.dequeue();
+  queue.dequeue();
+  print(queue);
+  queue.enqueue('Eric');
+  queue.enqueue('Eric1');
+  queue.enqueue('Eric2');
+  queue.enqueue('Eric3');
+  print(queue);
+  queue.dequeue();
+  print(queue);
+}
