af26051e127b680d4f39264fff7430194b55d922
Anthony Hoang <hoangtrungnguyen18102000@gmail.com>
2024-07-08 12:16:32 +0700

    unique characters problem

---
+++ lib/17_radix_sort/unique_characters.dart
@@ -0,0 +1,15 @@
+void main(){
+  final words = ['done', 'ad', 'eel', 'zoo', 'adept', 'do'];
+  print(uniqueCharacters(words));
+}
+
+int  uniqueCharacters(List<String> words){
+  final uniqueChars = <int>{};
+  for(final word in words){
+     for(final codeUnit in word.codeUnits){
+       uniqueChars.add(codeUnit);
+     }
+  }
+  return uniqueChars.length;
+}
+
